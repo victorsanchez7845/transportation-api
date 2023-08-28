e3f540fa5969b1d8988080d7f56d2815257087fe
caribbean-transfers <development@caribbean-transfers.com>
2023-08-28 18:24:21 -0500

    [UPDATE] Mailjet

---
+++ app/Traits/MailjetTrait.php
@@ -0,0 +1,58 @@
+
+<?php
+
+namespace App\Traits;
+use DateTime;
+
+trait MailjetTrait
+{
+    /**
+    * Para usar esta función, se necesita enviar la data que es un arreglo como este ejemplo:
+    *   $data = array(
+    *    "Messages" => array(
+    *        array(
+    *            "From" => array(
+    *                "Email" => "pilot@mailjet.com",
+    *                "Name" => "Mailjet Pilot"
+    *            ),
+    *            "To" => array(
+    *                array(
+    *                    "Email" => "passenger1@mailjet.com",
+    *                    "Name" => "passenger 1"
+    *                )
+    *            ),
+    *            "Subject" => "Your email flight plan!",
+    *            "TextPart" => "Dear passenger 1, welcome to Mailjet! May the delivery force be with you!",
+    *            "HTMLPart" => "<h3>Dear passenger 1, welcome to <a href=\"https://www.mailjet.com/\">Mailjet</a>!</h3><br />May the delivery force be with you!"
+    *        )
+    *    )
+    *  );
+    
+     */
+    public static function send( $data = [] ){
+        
+        $curl = curl_init();
+    
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.mailjet.com/v3.1/send',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => json_encode($data),
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'Authorization: Basic TUpfQVBJS0VZX1BVQkxJQzokTUpfQVBJS0VZX1BSSVZBVEU='
+            ),
+        ));
+        
+        $response = curl_exec($curl);
+        
+        curl_close($curl);
+        
+        echo $response;
+    }
+}
