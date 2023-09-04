edf8efd89435bf169fa3f56a10fee01c467d3b82
caribbean-transfers <development@caribbean-transfers.com>
2023-09-04 16:04:11 -0500

    Update transportation.blade.php

--- resources/views/mailing/transportation.blade.php
+++ resources/views/mailing/transportation.blade.php
@@ -206,7 +206,7 @@
                                             <tr>
                                                 <td><p style="font-weight:bold; font-size: 18pt;">{{$key}}</p></td>
                                                 <td rowspan="4" style="text-align:right;">
-                                                    <img src="https://api.caribbean-transfers.com/api/v1/reservation/qr?code={{$key}}" >
+                                                    <img src="{{config('app.url')}}/api/v1/reservation/qr?code={{$key}}" width="250">
                                                 </td>
                                             </tr>
                                             <tr>
