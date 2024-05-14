7380413e688ccff3b3a50598cf6c4c806b803fa1
caribbean-transfers <development@caribbean-transfers.com>
2024-05-14 11:27:46 -0500

    [UPDATE] Texto en correo
    Se agrega una descripción más detallada en los correos advirtiendo a los clientes de lo que les podrá suceder en el aeropuerto.

--- resources/views/mailing/transportation.blade.php
+++ resources/views/mailing/transportation.blade.php
@@ -90,6 +90,9 @@
             background-color: white;
             padding: 25px;
         }
+        td.white_content.information > p{
+            margin-bottom: 8px;
+        }
         p.label{
             font-weight: bold;
             margin-bottom: 8px;
@@ -314,11 +317,9 @@
                 </tr>
                 <tr>
                     <td class="white_content" style="border-top: 1px solid #CCD5D8; text-align:center;">
-                        @if($lang == "en")
-                            <p style="width: 70%; margin: 0 auto;">More information on how to find us here. Cancellation terms and conditions</p>
+                        @if($lang == "en")                            
                             <h3 style="margin-bottom: 0px; color: #191970;">Thank you for your reservation!</h3>
-                        @else
-                            <p style="width: 70%; margin: 0 auto;">Más información de cómo encontranos aquí. Términos y condiciones de cancelación</p>
+                        @else                            
                             <h4 style="margin-bottom: 0px; color: #191970;">¡Gracias por tu reservación!</h4>
                         @endif
                     </td>
@@ -331,7 +332,7 @@
                     </td>
                 </tr>
                 <tr>
-                    <td class="white_content important_information">
+                    <td class="white_content important_information" style="padding-top: 0px;">
                         @if( $mail->isNotEmpty() )
                             @foreach($mail as $key => $value)
                                 @php
@@ -342,21 +343,50 @@
                     </td>
                 </tr>
                 <tr>
-                    <td class="white_content" style="text-align:center;">
-                        @if($lang == "en")
-                            <p>We wish you a pleasant and unforgettable stay.</p>
-                            <h4>Policies</h4>
+                    <td class="white_content information" style="text-align:left; padding-top: 0px;">
+                        @if($lang == "en")                            
+                            <h4 style="color:red; text-align:center;">ITS VERY IMPORTANT TO YOU TO KNOW</h4>
+                            <h3>For ARRIVALS</h3>
+                            <p><strong>Upon arrival to the Airport please follow these recommendations to ensure an easy and fast access to your vehicle:</strong></p>
+                            <p>1.- When you arrive to the Airport, first you are going to go through immigrations, following you will be guided to the baggage claim area.</p>
+                            <p>2.- Once you have picked up your luggage you will be directed to Customs.</p>
+                            <p>3.- After you have cleared customs, please proceed to walk <strong>OUTSIDE</strong> your arrival terminal - <strong>it is very important to go all the way out since there will be large groups of "tourist advisors" which will try to stop you and claim to give you information about the destination however what they really do is sell time share, offering tours for free or discounted prices. For your own convenience please do not stop anywhere between customs and the airport exit.</strong></p>
+                            <p>4.- One of our representatives will be waiting for you and will gladly greet you so you can start enjoying your vacations. He will have a board with the Caribbean Transfers LOGO. This person will call your pick up transportation.</p>
+                            <p>5.- WE ARE ALWAYS AT THE AIRPORT, in case you can`t see us, please dial our phone numbers.</p>
+                            <p>6.- Tips for driver are <strong>NOT INCLUDED</strong>.</p>
+                            <p>IMPORTANT: <strong>DO NOT</strong> be fooled by others at the airport. Others may say they are with us or tell you they do not know us to steal your business. Our greeters are there all day, so you can ask security for help finding us, but beware of the pirates as they may tell you we are not there. We are <strong>ALWAYS THERE</strong>. </p>
+                            <p><strong>REMEMBER THAT WE WILL BE MONITORING YOUR FLIGHT IF IT IS DELAYED OR ARRIVED EARLY</strong>, it is not necessary to contact us, as we will be aware of this information. Only if your flight is canceled or changed, be sure to obtain the new flight information so that you can reschedule your transportation. </p>
+
+                            <h3>For DEPARTURES</h3>
+                            <p>The International Airport requests that all passengers departing on international flights must be at the airport at least 2 hours before the flight departure time. Your pick-up time was scheduled in accordance with this request and taking into account information such as flight departure time, estimated travel time between your hotel and airport, day of the week and assumed traffic flow.</p>
+
+                            <h4>Policies</h4>                            
                             <p class="gray_color">In case the service has been paid by credit card, you must present the card and your identification when boarding.</p>
-                            <h5>Cancellation Policy</h5> 
-                            <p class="gray_color">Cancellations can only be made 24 hours prior to arrival or departure.</p>                            
+                            <p class="gray_color">Cancellations can only be made 12 hours prior to arrival or departure.</p>
+
+
                             <h5>Service Hours</h5> 
                             <p class="gray_color">In case of change of time of service can be made if you contact us 12 hours before the agreed time because you have to reschedule your service. Contact us from 7AM to 11PM from Monday to Sunday at <a class="pink" href="tel:+529983870157">+52 (998) 387 0157</a> or email <a class="pink" href="mailto:bookings@caribbean-transfers.com">bookings@caribbean-transfers.com</a></p>
                         @else
-                            <p>Les deseamos que pasen unos dias gratamente inolvidables.</p>
+                            <h4 style="color:red; text-align:center;">ES MUY IMPORTANTE QUE SEPAS</h4>
+                            <h3>Para LLEGADAS</h3>
+                            <p><strong>A su llegada al aeropuerto, siga estas recomendaciones para garantizar un acceso fácil y rápido a su vehículo:</strong></p>
+                            <p>1.- Cuando llegue al aeropuerto, primero pasará por el control de inmigración y, a continuación, será conducido a la zona de recogida de equipajes.</p>
+                            <p>2.- Una vez que haya recogido su equipaje, se le dirigirá a la aduana.</p>
+                            <p>3.- Después de pasar la aduana, diríjase al <strong>EXTERIOR</strong> de la terminal de llegada - <strong>  es muy importante llegar hasta la salida, ya que habrá grandes grupos de "asesores turísticos" que intentarán pararle y afirmarán que le dan información sobre el destino, aunque lo que realmente hacen es vender tiempo compartido, ofreciendo visitas gratuitas o a precios rebajados. Por su propia comodidad, no se detenga entre la aduana y la salida del aeropuerto.</strong></p>
+                            <p>4.- Uno de nuestros representantes le estará esperando y gustosamente le dará la bienvenida para que pueda empezar a disfrutar de sus vacaciones. Él tendrá un tablero con el LOGO de Caribbean Transfers. Esta persona llamará a su transporte de recogida.</p>
+                            <p>5.- SIEMPRE ESTAMOS EN EL AEROPUERTO, en caso de que no pueda vernos, marque nuestros números de teléfono.</p>
+                            <p>6.- La propina para el conductor <strong>NO ESTÁ INCLUIDA</strong>.</p>
+                            <p>IMPORTANTE: <strong>NO</strong> se deje engañar por otras personas en el aeropuerto. Otros pueden decir que están con nosotros o decirle que no nos conocen para robarle su negocio. Nuestros recepcionistas están allí todo el día, por lo que puede pedir ayuda a seguridad para encontrarnos, pero tenga cuidado con los piratas, ya que pueden decirle que no estamos allí. Estamos <strong>SIEMPRE</strong>.</p>
+                            <p><strong>RECUERDE QUE ESTAREMOS PENDIENTES DE SU VUELO SI SE RETRASA O LLEGA ANTES DE LO PREVISTO</strong>, no es necesario que se ponga en contacto con nosotros, ya que tendremos conocimiento de esta información. Sólo si su vuelo se cancela o cambia, asegúrese de obtener la información del nuevo vuelo para poder reprogramar su transporte.</p>
+
+                            <h3>Para SALIDAS</h3>
+                            <p>El Aeropuerto Internacional solicita que todos los pasajeros que salgan en vuelos internacionales estén en el aeropuerto al menos 2 horas antes de la hora de salida del vuelo. Su hora de recogida se programó de acuerdo con esta petición y teniendo en cuenta información como la hora de salida del vuelo, la duración estimada del trayecto entre su hotel y el aeropuerto, el día de la semana y el flujo de tráfico previsto.</p>
+
                             <h4>Políticas</h4>
-                            <p class="gray_color">En caso de que el servicio haya sido pagado con tarjeta, deberá presentar la tarjeta y su identificación al abordar.</p>
-                            <h5>Politicas Para Cancelacion</h5> 
-                            <p class="gray_color">Unicamente se podra cancelar el servicio 24 hrs antes de su servicio sea llegada o salida.</p>
+                            <p class="gray_color">En caso de que el servicio haya sido pagado con tarjeta, deberá presentar la tarjeta y su identificación al abordar.</p>                            
+                            <p class="gray_color">Unicamente se podra cancelar el servicio 12 hrs antes de su servicio sea llegada o salida.</p>
+
                             <h5>Horario de Servicio</h5> 
                             <p class="gray_color">En caso de cambio de hora de servicio se podra efectuar si se comunica 12 hrs antes de la hora acordada ya que se tiene que reprogramar su servicio. Contáctanos de 7AM a 11PM de Lunes a Domingo al numero <a class="pink" href="tel:+529983870157">+52 (998) 387 0157</a> o al correo <a class="pink" href="mailto:bookings@caribbean-transfers.com">bookings@caribbean-transfers.com</a></p>
                         @endif
