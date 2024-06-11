ad011c468e9f573506f629001b6b1027f25eb9d4
victor daniel dorantes chay <victordorantes_1991@hotmail.com>
2024-06-11 12:47:40 -0500

    update endpoint spamChangeStatus

--- app/Repositories/Api/Spam/SpamRepository.php
+++ app/Repositories/Api/Spam/SpamRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories\Api\Spam;
 
 use App\Models\ReservationsItems;
+use App\Models\ReservationsFollowUp;
 use Illuminate\Support\Facades\DB;
 
 class SpamRepository{
@@ -10,6 +11,13 @@ class SpamRepository{
         $item = ReservationsItems::find($request->id);
         $item->spam = ( $request->status == 1 ? "ACCEPT" : ( $request->status == 2 ? "LATER" : "REJECTED" ) );
         $item->save();
+
+        $follow_up_db = new ReservationsFollowUp;
+        $follow_up_db->name = 'System';
+        $follow_up_db->text = "Automatización de llamada actualizado a (".( $request->status == 1 ? "ACCEPT" : ( $request->status == 2 ? "LATER" : "REJECTED" ) ).") ";
+        $follow_up_db->type = 'HISTORY';
+        $follow_up_db->reservation_id = $item->reservation_id;
+        $follow_up_db->save();
     }
 
     public function spamCallCount($request){
