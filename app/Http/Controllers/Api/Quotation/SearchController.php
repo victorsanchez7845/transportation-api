6139a69f46151fe2b1ec850de4a4084a10177e6f
FernandoDzay <luisf.dzay@gmail.com>
2025-12-25 14:21:21 -0600

    Separando cotizacion de tpv y sites

--- app/Http/Controllers/Api/Quotation/SearchController.php
+++ app/Http/Controllers/Api/Quotation/SearchController.php
@@ -51,10 +51,8 @@ class SearchController extends Controller
         }
 
         // -------------- Temporal
-        
-        
         $start_date = '2025-12-25 00:00';
-        $end_date   = '2025-12-26 00:00';
+        $end_date   = '2025-12-25 23:59';
 
         $one_way_date = Carbon::createFromFormat('Y-m-d H:i', $request['start']['pickup']);
         if(isset($request['end']['pickup'])) $round_trip_date = Carbon::createFromFormat('Y-m-d H:i', $request['end']['pickup']);
@@ -73,9 +71,118 @@ class SearchController extends Controller
                 ]
             ], 404);
         }
-
-        
         // -------------- Temporal
+
+        //Buscamos dentro de las geocercas existentes...
+        $availability = $search->findDestinations($request);
+        if($availability == false){            
+            return response()->json([
+                'error' => [
+                    'code' => 'availability',
+                    'message' => 'Sorry, we have no availability'
+                ]
+            ], 404);
+        }
+
+        //Buscamos tarifas disponibles por destino...
+        $prices = $rates->check($availability, $request);
+        if($prices == false){
+            return response()->json([
+                'error' => [
+                    'code' => 'rates',
+                    'message' => 'Rates not found'
+                ]
+            ], 404);
+        }
+
+        //Obtenemos el tiempo y distancia de diferencia entre el punto A y B
+        $geospacial = $distance->get($request, false);
+
+        $data = [
+            "places" => [
+                "one_way" => [
+                    "init" => [
+                        "name" => $request['start']['place'],
+                        "geo" => [
+                            "lat" => $request['start']['lat'],
+                            "lng" => $request['start']['lng'],
+                        ],
+                        "time" => $request['start']['pickup'],
+                    ],
+                    "end" => [
+                        "name" => $request['end']['place'],
+                        "geo" => [
+                            "lat" => $request['end']['lat'],
+                            "lng" => $request['end']['lng'],
+                        ],
+                        "time" => date("Y-m-d H:i", strtotime($request['start']['pickup']) + $geospacial['time_seconds'])
+                    ]
+                ],
+                "round_trip" => [
+                    "init" => [                        
+                        "name" => $request['end']['place'],
+                        "geo" => [
+                            "lat" => $request['end']['lat'],
+                            "lng" => $request['end']['lng'],
+                        ],
+                        "time" => ((isset( $request['end']['pickup'] ))? $request['end']['pickup'] : NULL),
+                    ],
+                    "end" => [
+                        "name" => $request['start']['place'],
+                        "geo" => [
+                            "lat" => $request['start']['lat'],
+                            "lng" => $request['start']['lng'],
+                        ],
+                        "time" => ((isset( $request['end']['pickup'] ))? date("Y-m-d H:i", strtotime($request['end']['pickup']) + $geospacial['time_seconds']) : NULL)                        
+                    ]
+                ],
+                "distance" => $geospacial['distance'],
+                "time" => $geospacial['time'],
+                "config" => [
+                    "flight_required" => (($availability['start']['data']['zone']['is_primary'] == 1)? true : false),
+                    "iata_code" => (( isset($availability['start']['data']['zone']['iata_code']) && !empty($availability['start']['data']['zone']['iata_code']) )? $availability['start']['data']['zone']['iata_code'] : NULL)
+                ]
+            ],
+            "items" => $prices
+        ];
+        
+        if($request['type'] == "one-way"){
+            unset( $data['places']['round_trip'] );
+        }
+        
+        return response()->json($data, 200);
+    }
+
+    public function quoteTpv(Request $request, SearchRepository $search, RatesRepository $rates, DistanceRepository $distance){    
+        
+        $validator = Validator::make($request->all(), [
+            'type' => 'required|in:one-way,round-trip',
+            'start.lat' => 'required',
+            'start.lng' => 'required',
+            'start.place' => 'required|max:150',            
+            'start.pickup' => 'required|date_format:Y-m-d H:i',
+            'end.lat' => 'required',
+            'end.lng' => 'required',
+            'end.place' => 'required|max:150',
+            'end.pickup' => [
+                'required_if:type,round-trip',
+                'date_format:Y-m-d H:i',
+            ],
+            'language' => 'required|in:en,es',
+            'passengers' => 'required|integer|min:1|max:150',
+            'currency' => 'required|in:USD,MXN',
+            'rate_group' => 'required|max:10',
+            'service' => 'integer',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => [
+                    'code' => 'required_params',
+                    'message' => $validator->errors()->all() 
+                ]
+            ], 422);
+        }
 
         //Buscamos dentro de las geocercas existentes...
         $availability = $search->findDestinations($request);
