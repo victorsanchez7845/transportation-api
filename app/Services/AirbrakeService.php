23dbc70b6c1a0d54da98871737de37b53cd4d20e
victorsanchez7845 <59267510+victorsanchez7845@users.noreply.github.com>
2026-04-09 21:08:27 -0600

    Desactivando  Airbreak

--- app/Services/AirbrakeService.php
+++ app/Services/AirbrakeService.php
@@ -10,34 +10,26 @@ class AirbrakeService
 
     public function __construct()
     {
-        // Initialize the Airbrake Notifier
-if (config('services.airbrake.id') && config('services.airbrake.key')) {
-    $this->notifier = new Notifier([
-        'projectId' => config('services.airbrake.id'),
-        'projectKey' => config('services.airbrake.key'),
-        'environment' => 'production',
-    ]);
-}
-
-    /**
-     * Report an exception to Airbrake.
-     *
-     * @param \Throwable $exception
-     * @return void
-     */
-    public function report(\Throwable $exception)
-    {
-        $this->notifier->notify($exception);
+        if (config('services.airbrake.id') && config('services.airbrake.key')) {
+            $this->notifier = new Notifier([
+                'projectId' => config('services.airbrake.id'),
+                'projectKey' => config('services.airbrake.key'),
+                'environment' => 'production',
+            ]);
+        }
     }
 
-    /**
-     * Report a custom error message to Airbrake.
-     *
-     * @param string $message
-     * @return void
-     */
+    public function report(\Throwable $exception)
+    {
+        if ($this->notifier) {
+            $this->notifier->notify($exception);
+        }
+    }
+
     public function reportMessage(string $message)
     {
-        $this->notifier->notify(new \Exception($message));
+        if ($this->notifier) {
+            $this->notifier->notify(new \Exception($message));
+        }
     }
 }
